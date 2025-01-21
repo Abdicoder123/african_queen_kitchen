c008a4a4d9d31ab0412c621c610b1f309f14111a
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2025-01-21 11:56:44 -0600

    Generated reviews model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.2].define(version: 2025_01_18_004846) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_21_174923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dishes", force: :cascade do |t|
+    t.integer "menu_id"
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.integer "price"
+    t.string "customizable"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "invoices", force: :cascade do |t|
     t.bigint "order_id"
@@ -34,6 +44,17 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_18_004846) do
     t.text "special_notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "dish_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "quantity"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dish_id"], name: "index_order_items_on_dish_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -58,6 +79,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_18_004846) do
     t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "dish_id"
+    t.integer "user_id"
+    t.string "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,5 +103,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_18_004846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "dishes"
+  add_foreign_key "order_items", "orders"
   add_foreign_key "orders", "users"
 end
