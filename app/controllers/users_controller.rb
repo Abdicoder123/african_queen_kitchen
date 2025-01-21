a08d58e815e0ce494814d593836aae4699f0f8dc
Torshia Lee <torshwlee@gmail.com>
2025-01-20 22:02:35 -0600

    Ran a migration to add stripe_customer_id and added logic to user.rb to create a customer and save their stripe id in our database.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,32 @@
 class UsersController < ApplicationController
+
+
+    # Callbacks to handle creating and deleting Stripe customers
+    after_create :create_stripe_customer
+    before_destroy :delete_stripe_customer
+
     private
       def user_params
           params.require(:user).permit(:name, :address, :phone_number, :email)
       end
+
+      def create_stripe_customer
+        if stripe_customer_id.blank?
+          customer = Stripe::Customer.create(  #Creates a Stripe customer with the user model information
+            email: email, 
+            name: name,
+            phone: phone_number,
+          )
+          self.stripe_customer_id = customer.id #Sets the stripe_customer_id in database to customer.id (customer.id is returned from stripe after creating the customer)
+          save
+        end
+      end
+
+      def delete_stripe_customer #Deletes the stripe customer if user deletes their account on the app
+        if stripe_customer_id.present?
+          Stripe::Customer.delete(stripe_customer_id)
+        end
+      end
+    
+    
 end
