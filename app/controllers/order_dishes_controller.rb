0f2c510298c075d40b8121b1a92455fc3c25aefe
Torshia Lee <torshwlee@gmail.com>
2025-01-22 17:47:15 -0600

    updated CRUD for order_dish to only be available depending on order status

---
+++ app/controllers/order_dishes_controller.rb
@@ -0,0 +1,23 @@
+class OrderDishesController < ApplicationController
+    
+    def new
+        @order_dish = Order_dish.new
+    end
+    
+    def create
+        @order = Order.find(params[:order_id])
+        @order_dish = @order.order_dishes.create(order_dish_params)
+
+        if @order_dish.save
+          redirect_to order_path(@order), notice: "Dish was added successfully."
+        else
+          redirect_to order_path(@order), notice: "Make sure you fill in the form completely!"
+        end
+    end
+
+    private
+    # Only allow a list of trusted parameters through.
+    def order_dish_params
+      params.require(:order_dish).permit(:dish_id, :quantity)
+    end
+end
