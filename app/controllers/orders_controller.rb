9083eab46ad244e7a0f7663bd7ab857f8eb8d478
Torshia Lee <torshwlee@gmail.com>
2025-01-22 14:33:38 -0600

    Create different links for order history and new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,36 +1,41 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @orders = Order.all
+    @orders = Order.order(created_at: :desc)
+
   end
   
   def show
     @order = Order.find(params[:id])
+    @order_dishes = @order.order_dishes
+    @dishes = @order.dishes
   end
 
   def new
     @order = Order.new 
+    @order.order_dishes.build 
   end
 
   def create
     @order = current_user.orders.new(order_params)
-
+  
     respond_to do |format|
       if @order.save
-        format.html { redirect_to orders_path, notice: "Order was successfully created." }
+        format.html { redirect_to @order, notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
-        puts @order.errors.full_messages
+        # Log error messages for debugging
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
   
+  
 
   private
 
   def order_params
-    params.require(:order).permit(:delivery_date, :status, :event_details, :group_size, :total_cost, order_items_attributes: [:dish_id, :quantity, :total_price, :_destroy])
+    params.require(:order).permit(:delivery_date, :status, :event_details, :group_size, :total_cost, order_dishes_attributes: [:dish_id, :quantity, :total_price, :_destroy])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,12 @@ class Order < ApplicationRecord
     has_many :order_dishes, dependent: :destroy
     has_many :dishes, through: :order_dishes
 
-
-    before_save :set_pending_status
+    before_save :set_creation_status #Sets the status of the order automatically to pending when user submits
 
 
     private
 
-    def set_pending_status
-        self.status ||= "Pending"
+    def set_creation_status
+        self.status ||= "Order Creation"
     end
 end
