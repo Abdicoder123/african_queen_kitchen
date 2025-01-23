c03fb7d745ea437709daffa7d01b9dfbf6b021ed
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2025-01-23 10:54:22 -0600

    Did the set up for the full crud for review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,59 +1,52 @@
 class ReviewsController < ApplicationController
-        before_action :set_review, only: [ :show, :edit, :update, :destroy ]
-        before_action :set_dish, only: [ :new, :create ]
-
-        def index
-          @reviews = Review.all
-        end
-
-        def show
-            @review
-        end
+    before_action :set_review, only: [ :show, :edit, :update, :destroy ]
 
 
-        def new
-            @review = @dish.reviews.build
-        end
+    def index
+      @reviews = Review.all
+    end
 
+    def show
+    end
 
-        def create
-          @review = @dish.reviews.build(review_params)
-          @review.user_id = current_user.id
+    def new
+      @review = Review.new
+    end
 
-          if @review.save
-            redirect_to @dish, notice: "Review was successfully created."
-          else
-            render :new
-          end
-        end
+    def create
+      @review = Review.new(review_params)
+      @review.user_id = current_user.id
 
-        def edit
-            @review
-        end
+      if @review.save
+        redirect_to reviews_path, notice: "Review was successfully created."
+      else
+        render :new
+      end
+    end
 
-        def update
-          if @review.update(review_params)
-            redirect_to @review, notice: "Review was successfully updated."
-          else
-            render :edit
-          end
-        end
+    def edit
+    end
 
-        def destroy
-          @review.destroy
-          redirect_to @review.dish, notice: "Review was successfully destroyed."
-        end
+    def update
+      if @review.update(review_params)
+        redirect_to @review, notice: "Review was successfully updated."
+      else
+        render :edit
+      end
+    end
 
-        private
-          def set_review
-            @review = Review.find(params[:id])
-          end
+    def destroy
+      @review.destroy
+      redirect_to reviews_path, notice: "Review was successfully destroyed."
+    end
 
-          def set_dish
-            @dish = Dish.find(params[:dish_id])
-          end
+    private
 
-          def review_params
-            params.require(:review).permit(:rating, :comment)
-          end
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
 end
