83f19c198b3820ea6844a2290d7c794e6149e6a6
Torshia Lee <torshwlee@gmail.com>
2025-01-24 11:30:31 -0600

    Modified order controller to only show order instances belonging to current user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   def index
-    @orders = Order.order(created_at: :desc)
+    @orders = current_user.orders.order(created_at: :desc)
   end
 
   def show
