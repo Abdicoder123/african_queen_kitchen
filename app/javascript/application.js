20739900221b2da6cf8a00483cec8d2dfe424a33
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2025-01-25 07:53:23 -0600

    Made sure that you cannot drop a review if you're not signed in and fixed the bug

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+import "bootstrap"  // Ensure bootstrap.js is loaded here
+
+Turbo.session.drive = false;
