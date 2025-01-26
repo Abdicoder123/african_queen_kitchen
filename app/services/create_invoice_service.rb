10fbe792a9df06c6cbd8b1aac34e3735a0aff356
Torshia Lee <torshwlee@gmail.com>
2025-01-26 15:58:17 -0600

    Rearranged logic to match main

--- app/services/create_invoice_service.rb
+++ app/services/create_invoice_service.rb
@@ -20,10 +20,6 @@ class CreateInvoiceService
     if stripe_invoice
       create_invoice_items(stripe_invoice)
       save_invoice(user, stripe_invoice)
-
-      # Send the invoice email to the user associated with this invoice
-      # Finalize the invoice so it gets sent out
-      Stripe::Invoice.finalize_invoice(stripe_invoice.id)
     end
   end
 
