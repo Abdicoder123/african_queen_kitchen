28b79e58bbae55245235162cd4be23a6f87ed56b
Julie Lee <julielee@Julies-MacBook-Air.local>
2025-01-26 16:32:02 -0600

    fixing total cost to take in decimals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_26_170717) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_26_222933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,7 +93,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_26_170717) do
     t.string "status"
     t.text "event_details"
     t.integer "group_size"
-    t.decimal "total_cost"
+    t.decimal "total_cost", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
