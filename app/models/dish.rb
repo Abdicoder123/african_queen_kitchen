cf4e7647be7364c80121af4ccd9c6188c6121496
Abdirahman <abdirahman@Abdirahmans-MacBook-Air-2.local>
2025-01-27 12:43:17 -0600

    Just fixed minor issues

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,6 @@
 class Dish < ApplicationRecord
     belongs_to :menu
-    belongs_to :admin_user
+    
 
     has_many :order_dishes, dependent: :destroy
     has_many :orders, through: :order_dishes
