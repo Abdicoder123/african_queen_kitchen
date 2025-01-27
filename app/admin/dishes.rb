53a28d78a168673b22b027a8646f830882ca26d9
Tamir Harris <tamirharris@Tamirs-Air.attlocal.net>
2025-01-27 11:00:33 -0600

    committing to merge from main

--- app/admin/dishes.rb
+++ app/admin/dishes.rb
@@ -1,30 +1,49 @@
 ActiveAdmin.register Dish do
-  permit_params :menu_id, :title, :description, :image_url, :price, :customizable
+  permit_params :menu_id, :title, :description, :price, :customizable, images: []
 
   index do
     selectable_column
     id_column
     column :title
     column :description
-    column :image_url
     column :price
     column :customizable
     column :created_at
     column :updated_at
+    column "Images" do |dish|
+      if dish.images.attached?
+        dish.images.map do |image|
+          image_tag image, size: "50x50" # Thumbnail preview
+        end.join(" ").html_safe
+      else
+        "No images uploaded"
+      end
+    end
     actions
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :menu_id, :title, :description, :image_url, :price, :customizable
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:menu_id, :title, :description, :image_url, :price, :customizable]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  form do |f|
+    f.inputs "Dish Details" do
+      f.input :title
+      f.input :description
+      f.input :price
+      f.input :customizable
+      f.input :menu_id, as: :select, collection: Menu.all.pluck(:title, :id)
+      f.input :images, as: :file, input_html: { multiple: true }
+    end
+    f.actions
+  end
+
+  filter :title
+  filter :description
+  filter :price
+  filter :customizable
+  filter :created_at
+
+  # Prevent ActiveAdmin from creating default filters for images
+  controller do
+    def scoped_collection
+      super.includes(:images_attachments) # Optimize queries for image display
+    end
+  end
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,7 @@ class Dish < ApplicationRecord
     belongs_to :menu
     has_many :order_dishes, dependent: :destroy
     has_many :orders, through: :order_dishes
+    has_many_attached :images
 
     def self.ransackable_attributes(auth_object = nil)
         [ "created_at", "customizable", "description", "id", "image_url", "menu_id", "price", "title", "updated_at" ]
