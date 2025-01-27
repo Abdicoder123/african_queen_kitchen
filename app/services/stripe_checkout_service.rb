345eff4ee8f0e9efda2195a02a0bca7c1a5600fd
Torshia Lee <torshwlee@gmail.com>
2025-01-27 11:10:55 -0600

    Attach invoice ID to checkout session

--- app/services/stripe_checkout_service.rb
+++ app/services/stripe_checkout_service.rb
@@ -8,6 +8,7 @@ class StripeCheckoutService
         session = Stripe::Checkout::Session.create({
         payment_method_types: [ "card" ],
         customer: @user.stripe_customer_id,
+        invoice: @invoice
         line_items: [
           {
             price_data: {
