630807054c40e8f5978cc2c4588b41174401f822
Torshia Lee <torshwlee@gmail.com>
2025-01-27 08:24:39 -0600

    Ran rubocop

--- app/controllers/order_dishes_controller.rb
+++ app/controllers/order_dishes_controller.rb
@@ -6,16 +6,16 @@ class OrderDishesController < ApplicationController
     def create
         @order = Order.find(params[:order_id])
         @order_dish = @order.order_dishes.create(order_dish_params)
-       
+
 
         if @order_dish.save
           @dish_price = @order_dish.dish.price
 
           total_price = @order_dish.quantity * @dish_price
-          
+
           # Update the order_dish with the calculated total_price
           @order_dish.update(price: total_price)
-          
+
           redirect_to edit_order_path(@order), notice: "Dish was added successfully."
         else
           redirect_to edit_order_path(@order), notice: "Make sure you fill in the form completely!"
