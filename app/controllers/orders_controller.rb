2d81dfa57f99ed1072f6a9dbac1f2805920d0781
Torshia Lee <torshwlee@gmail.com>
2025-01-26 18:17:00 -0600

    Ran rubocop

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,12 +29,12 @@ class OrdersController < ApplicationController
 
     # Redirect to stripe checkout session logic
     if @invoice.invoice_status === "Payment Pending" && @order.status == "Confirmed"
-      if @invoice.stripe_checkout_session_id.blank? #Checks if a checkout already exists for that invoice
+      if @invoice.stripe_checkout_session_id.blank? # Checks if a checkout already exists for that invoice
         service = StripeCheckoutService.new(@invoice, @user)
         session = service.create_checkout_session
         redirect_to session.url, allow_other_host: true
       else
-        fetch_checkout_session  #If a checkout already exists
+        fetch_checkout_session  # If a checkout already exists
       end
     end
   end
@@ -120,7 +120,7 @@ class OrdersController < ApplicationController
   def fetch_checkout_session
     # Retrieve the existing session
     session = Stripe::Checkout::Session.retrieve(@invoice.stripe_checkout_session_id)
-    
+
     # Redirect the user to the existing session's URL
     redirect_to session.url, allow_other_host: true
   end
