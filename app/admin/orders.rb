9b08a8037e4f440cec4dfda23d20ded32f8d0618
Julie Lee <julielee@Julies-MacBook-Air.local>
2025-01-29 11:45:07 -0600

    fixed spelling error for cancelled

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -202,7 +202,7 @@ ActiveAdmin.register Order do
       end
     end
     f.inputs "Update Order Status" do
-      f.input :status, as: :select, collection: %w[Pending Confirmed Shipped Completed Canceled], include_blank: true
+      f.input :status, as: :select, collection: %w[Pending Confirmed Shipped Completed Cancelled], include_blank: true
     end
     f.actions
   end
