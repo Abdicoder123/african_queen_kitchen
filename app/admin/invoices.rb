d8c45fa68039536f653a337fc9e350c379271ae1
Julie Lee <julielee@Julies-MacBook-Air.local>
2025-01-29 15:03:56 -0600

    updated amount due column for invoices

--- app/admin/invoices.rb
+++ app/admin/invoices.rb
@@ -6,15 +6,20 @@ ActiveAdmin.register Invoice do
       div do
         h3 "Here is where all of the invoices for orders are stored."
         h3 "Once you can confirm an order, an invoice is automatically created and will appear here."
-        h3 "The 'Amount Due', 'Currency' used, and 'Status' of the invoice payment is shown here."
         h3 "This is the place you can refer to check if an order has been paid for."
+        h3 "During 'Draft' the invoice will not yet amount to anything."
+        h3 "If you cancel an order, the status will be 'Deleted'."
+        h3 "Once you accept and confirm an order, the amount due will show up."
       end
     end
 
     column :id
     column :user
     column :stripe_invoice_id
-    column :amount_due
+    column :amount_due do |invoice|
+      total = invoice.total_amount
+      number_to_currency(total)
+    end
     column :currency
     column :invoice_status
     column :created_at
