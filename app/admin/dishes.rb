33b602033adf7c280d69e35e8e177702e1cd2d97
Julie Lee <julielee@Julies-MacBook-Air.local>
2025-01-30 14:47:18 -0600

    added menu and dish information panels for admin

--- app/admin/dishes.rb
+++ app/admin/dishes.rb
@@ -2,11 +2,22 @@ ActiveAdmin.register Dish do
   permit_params :menu_id, :title, :description, :price, :customizable, images: []
 
   index do
+    panel "Dish Overview" do
+      div do
+        h3 "This is where all of your dishes are!"
+        h3 "You can view, edit and delete every dish as you see fit by clicking the respective links."
+        h3 "Upon clicking edit, you can edit any information you need to and update/add a photo."
+        h3 "In the upper right corner, you can click 'New Dish' to create new dishes you want to add and even select which menu they belong to."
+      end
+    end
+
     selectable_column
     id_column
     column :title
     column :description
-    column :price
+    column :price do |dish|
+      number_to_currency(dish.price)
+    end
     column :customizable
     column :created_at
     column :updated_at
